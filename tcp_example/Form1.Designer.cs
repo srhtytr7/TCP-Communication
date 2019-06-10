7a3db84ca04fc7001c3dffe20e6a433bd486b482
Serhat Yatar <srhtytr7@gmail.com>
2019-06-10 11:52:57 +0300

    Add files via upload

---
+++ tcp_example/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace tcp_example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonServerToClient = new System.Windows.Forms.Button();
+            this.buttonClientToServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 63);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(216, 290);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(363, 63);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(216, 290);
+            this.listBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(84, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(445, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Client";
+            // 
+            // buttonServerToClient
+            // 
+            this.buttonServerToClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonServerToClient.Location = new System.Drawing.Point(262, 141);
+            this.buttonServerToClient.Name = "buttonServerToClient";
+            this.buttonServerToClient.Size = new System.Drawing.Size(75, 42);
+            this.buttonServerToClient.TabIndex = 2;
+            this.buttonServerToClient.Text = ">>";
+            this.buttonServerToClient.UseVisualStyleBackColor = true;
+            this.buttonServerToClient.Click += new System.EventHandler(this.buttonServerToClient_Click);
+            // 
+            // buttonClientToServer
+            // 
+            this.buttonClientToServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonClientToServer.Location = new System.Drawing.Point(262, 234);
+            this.buttonClientToServer.Name = "buttonClientToServer";
+            this.buttonClientToServer.Size = new System.Drawing.Size(75, 42);
+            this.buttonClientToServer.TabIndex = 2;
+            this.buttonClientToServer.Text = "<<";
+            this.buttonClientToServer.UseVisualStyleBackColor = true;
+            this.buttonClientToServer.Click += new System.EventHandler(this.buttonClientToServer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 404);
+            this.Controls.Add(this.buttonClientToServer);
+            this.Controls.Add(this.buttonServerToClient);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonServerToClient;
+        private System.Windows.Forms.Button buttonClientToServer;
+    }
+}
+
---
+++ tcp_example/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace tcp_example
+{
+    public partial class Form1 : Form
+    {
+        TCP_Ex tcp1 = new TCP_Ex();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        { // filling listboxes
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add(rnd.Next(1000, 10000));
+                listBox2.Items.Add(rnd.Next(1000, 10000));
+            }
+        }
+
+        private void buttonServerToClient_Click(object sender, EventArgs e)
+        {
+            object item = listBox1.SelectedItem;
+            if (item != null)
+            {
+                tcp1.sendServerToClient(item);// sending the selected data
+                listBox2.Items.Add(tcp1.receiveFromServer());//receiving the data and adding to relevant listbox
+                listBox1.Items.Remove(item); //removing the data from other listbox
+            }
+        }
+
+        private void buttonClientToServer_Click(object sender, EventArgs e)
+        { //sending the other way
+            object item = listBox2.SelectedItem;
+            if (item != null)
+            {
+                tcp1.sendServerToClient(item);
+                listBox1.Items.Add(tcp1.receiveFromServer());
+                listBox2.Items.Remove(item);
+            }
+        }
+    }
+
+    public class TCP_Ex
+    {   //my class for tcp communication
+        TcpListener server = new TcpListener(IPAddress.Any, 2500); // server listens from any ip
+        TcpClient client = new TcpClient();
+        NetworkStream streamServer;
+        NetworkStream streamClient;
+        TcpClient acceptedTCPClient;
+
+        public TCP_Ex()
+        {
+            server.Start();
+            client.Connect(IPAddress.Parse("127.0.0.1"), 2500);
+            acceptedTCPClient = server.AcceptTcpClient();
+            streamServer = acceptedTCPClient.GetStream();
+            streamClient = client.GetStream();
+        }
+        public void sendServerToClient(object item)
+        {
+            //send item to client from server
+            streamServer.Write(Encoding.ASCII.GetBytes(item.ToString()), 0, item.ToString().Length);
+        }
+        public object receiveFromServer()
+        {
+            //receive item from server
+            byte[] item = new byte[10];
+            streamClient.Read(item, 0, item.Length);
+            return Encoding.ASCII.GetString(item);
+        }
+        public void sendClientToServer(object item)
+        {
+            //send item to client from server
+            streamClient.Write(Encoding.ASCII.GetBytes(item.ToString()), 0, item.ToString().Length);
+        }
+        public object receiveFromClient()
+        {
+            //receive item from client
+            byte[] item = new byte[acceptedTCPClient.ReceiveBufferSize];
+            streamServer.Read(item, 0, item.Length);
+            return Encoding.ASCII.GetString(item);
+        }
+    }
+}
